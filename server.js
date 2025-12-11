a437fd6e2541631c3f64427e15f42898d6f88bee
iamWilliam <anh.tran3@uta.edu>
2025-12-11 10:44:45 -0600

    fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 // Middleware to parse JSON data
-app.use(express.json());
+//app.use(express.json());
 //Method
 app.get('/webhook',(req,res)=>{
     console.log("Received request:", req.body);
